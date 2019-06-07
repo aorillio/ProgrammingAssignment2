b401f8a64e0ab4bb3eda7439af2ea98bdadf7b49
AOrillion <50183904+aorillio@users.noreply.github.com>
2019-06-07 12:35:42 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -29,3 +29,23 @@ cacheSolve <- function(x, ...) {
   x$setInverse(inv)
   inv
 }
+
+
+##Testing##
+
+> TestMatrix <- matrix(1:4, 2, 2)
+> TestMatrix
+     [,1] [,2]
+[1,]    1    3
+[2,]    2    4
+
+ CacheMatrix <- makeCacheMatrix(TestMatrix)
+> CacheMatrix$get()
+     [,1] [,2]
+[1,]    1    3
+[2,]    2    4
+
+> cacheSolve(CacheMatrix)
+     [,1] [,2]
+[1,]   -2  1.5
+[2,]    1 -0.5
